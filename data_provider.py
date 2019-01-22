d5951c0bccd2f8d24a808c1afa441e2cac9e118d
florian-kalisch <florian-kalisch@users.noreply.github.com>
2019-01-22 17:41:12 +0100

    Ported to Python 3

--- data_provider.py
+++ data_provider.py
@@ -11,11 +11,11 @@ class DataProvider:
         self.sequence_length = sequence_length
         with codecs.open(os.path.join(data_dir, "input.txt"), "r", encoding="utf-8") as file:
             data = file.read()
-        count_pairs = sorted(collections.Counter(data).items(), key=lambda x: -x[1])
+        count_pairs = sorted(list(collections.Counter(data).items()), key=lambda x: -x[1])
         self.pointer = 0
-        self.chars, _ = zip(*count_pairs)
+        self.chars, _ = list(zip(*count_pairs))
         self.vocabulary_size = len(self.chars)
-        self.vocabulary = dict(zip(self.chars, range(len(self.chars))))
+        self.vocabulary = dict(list(zip(self.chars, list(range(len(self.chars))))))
         self.tensor = np.array(list(map(self.vocabulary.get, data)))
         self.batches_size = int(self.tensor.size / (self.batch_size * self.sequence_length))
         if self.batches_size == 0:
@@ -29,11 +29,11 @@ class DataProvider:
         targets[-1] = inputs[0]
         self.input_batches = np.split(inputs.reshape(self.batch_size, -1), self.batches_size, 1)
         self.target_batches = np.split(targets.reshape(self.batch_size, -1), self.batches_size, 1)
-        print "Tensor size: " + str(self.tensor.size)
-        print "Batch size: " + str(self.batch_size)
-        print "Sequence length: " + str(self.sequence_length)
-        print "Batches size: " + str(self.batches_size)
-        print ""
+        print("Tensor size: " + str(self.tensor.size))
+        print("Batch size: " + str(self.batch_size))
+        print("Sequence length: " + str(self.sequence_length))
+        print("Batches size: " + str(self.batches_size))
+        print("")
 
     def next_batch(self):
         inputs = self.input_batches[self.pointer]
